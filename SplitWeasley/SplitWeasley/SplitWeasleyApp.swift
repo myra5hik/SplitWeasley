a7a78a062871237bfb1346c4884dde3cee92fff7
Alexander Tokarev <atokarev22@fed.idserve.net>
2022-11-16 21:52:04 +0100

    - Empty Xcode project

---
+++ SplitWeasley/SplitWeasley/SplitWeasleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitWeasleyApp.swift
+//  SplitWeasley
+//
+//  Created by Alexander Tokarev on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitWeasleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
