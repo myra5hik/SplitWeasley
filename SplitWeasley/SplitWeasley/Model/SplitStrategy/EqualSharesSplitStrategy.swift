a48745f67e3f1e6d001a846398cf10c756c28057
Alexander Tokarev <atokarev22@fed.idserve.net>
2022-11-23 16:30:00 +0100

    - Cut SplitStrategy into separate files

--- SplitWeasley/SplitWeasley/Model/SplitStrategy/EqualSharesSplitStrategy.swift
+++ SplitWeasley/SplitWeasley/Model/SplitStrategy/EqualSharesSplitStrategy.swift
@@ -1,28 +1,11 @@
 //
-//  SplitStrategy.swift
+//  EqualSharesSplitStrategy.swift
 //  SplitWeasley
 //
-//  Created by Alexander Tokarev on 21/11/22.
+//  Created by Alexander Tokarev on 23/11/22.
 //
 
 import Foundation
-
-protocol ISplitStrategy: ObservableObject {
-    /// Signifies that the applied parameters are consistent with the logic of the app. If false, views should not allow for further processing.
-    var isLogicallyConsistent: Bool { get }
-    /// Heading used by views to describe the split strategy
-    var hintHeader: String { get }
-    /// Text body used by views to describe the split strategy
-    var hintDescription: String { get }
-    /// One-word stretegy description used by views
-    var conciseHintDescription: String { get }
-    // Init
-    init(splitGroup: SplitGroup, total: MonetaryAmount)
-    /// Returns the amount split towards the person passed as parameter.
-    func amount(for personId: Person.ID) -> MonetaryAmount?
-}
-
-// MARK: - EqualSharesSplitStrategy
 
 protocol IEqualSharesSplitStrategy: ISplitStrategy {
     var isIncluded: [Person.ID: Bool] { get set }
