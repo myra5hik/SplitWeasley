b67a674eba815862b612cf098c0c3812415b8bb6
Alexander Tokarev <atokarev22@fed.idserve.net>
2023-01-25 23:45:02 +0100

    - Extended TransactionCategory with Home and Life categories - Automated TransactionCategory.description generation

--- SplitWeasley/SplitWeasley/Model/TransactionCategory.swift
+++ SplitWeasley/SplitWeasley/Model/TransactionCategory.swift
@@ -22,14 +22,44 @@ enum TransactionCategory: String, CaseIterable, Hashable, Identifiable, Codable 
     case groceries
     case liquor
     case otherFoodAndDrink
+    // Home
+    case electronics
+    case furniture
+    case householdSupplies
+    case maintenance
+    case mortgage
+    case pets
+    case rent
+    case services
+    case otherHome
+    // Life
+    case childcare
+    case clothing
+    case education
+    case gifts
+    case insurance
+    case medicalExpenses
+    case taxes
+    case otherLife
     // Undefined
     case undefined
 
     var grouping: Grouping {
         switch self {
-        case .games, .movies, .music, .sports, .otherEntertainment: return .entertainment
-        case .diningOut, .groceries, .liquor, .otherFoodAndDrink: return .foodAndDrink
-        case .undefined: return .undefined
+        // Entertainment
+        case .games, .movies, .music, .sports, .otherEntertainment:
+            return .entertainment
+        // Food and Drink
+        case .diningOut, .groceries, .liquor, .otherFoodAndDrink:
+            return .food
+        // Home
+        case .electronics, .furniture, .householdSupplies, .maintenance, .mortgage, .pets, .rent, .services, .otherHome:
+            return .home
+        // Undefined
+        case .undefined:
+            return .undefined
+        case .childcare, .clothing, .education, .gifts, .insurance, .medicalExpenses, .taxes, .otherLife:
+            return .life
         }
     }
 
@@ -46,6 +76,25 @@ enum TransactionCategory: String, CaseIterable, Hashable, Identifiable, Codable 
         case .groceries: return Image(systemName: "basket.fill")
         case .liquor: return Image(systemName: "wineglass.fill")
         case .otherFoodAndDrink: return Self.otherIcon
+        // Home
+        case .electronics: return Image(systemName: "tv")
+        case .furniture: return Image(systemName: "sofa.fill")
+        case .householdSupplies: return Image(systemName: "shower")
+        case .maintenance: return Image(systemName: "hammer.fill")
+        case .mortgage: return Image(systemName: "house.fill")
+        case .pets: return Image(systemName: "pawprint.fill")
+        case .rent: return Image(systemName: "house.fill")
+        case .services: return Image(systemName: "bell.fill")
+        case .otherHome: return Image(systemName: "house.fill")
+        // Life
+        case .childcare: return Image(systemName: "figure.and.child.holdinghands")
+        case .clothing: return Image(systemName: "figure.dance")
+        case .education: return Image(systemName: "book.fill")
+        case .gifts: return Image(systemName: "gift.fill")
+        case .insurance: return Image(systemName: "doc.append.fill")
+        case .medicalExpenses: return Image(systemName: "medical.thermometer.fill")
+        case .taxes: return Image(systemName: "building.columns.fill")
+        case .otherLife: return Image(systemName: "heart.fill")
         // Undefined
         case .undefined: return Self.otherIcon
         }
@@ -64,11 +113,17 @@ enum TransactionCategory: String, CaseIterable, Hashable, Identifiable, Codable 
     }
 
     var description: String {
-        switch self {
-        case .otherEntertainment, .otherFoodAndDrink: return "Other"
-        case .diningOut: return "Dining Out"
-        default: return self.rawValue.capitalized
+        let caseString = "\(self)"
+        // Returns a concise "Other" for all 'other...' cases
+        if caseString.hasPrefix("other") { return "Other" }
+        // Returns a string with each word capitalized, as split by camelCase convention
+        var res = [""]
+        for c in caseString {
+            if c.isUppercase { res.append("") }
+            res[res.endIndex - 1].append(String(c))
         }
+
+        return String(res.map({ $0.capitalized }).joined(separator: " "))
     }
 }
 
@@ -78,15 +133,19 @@ extension TransactionCategory {
     enum Grouping: String, CaseIterable, Hashable, Identifiable, Codable {
         var id: String { self.rawValue }
 
-        case entertainment
-        case foodAndDrink
         case undefined
+        case entertainment
+        case food
+        case home
+        case life
 
         var color: Color {
             switch self {
-            case .entertainment: return Color(uiColor: .systemPurple).opacity(0.75)
-            case .foodAndDrink: return Color(uiColor: .systemGreen).opacity(0.75)
             case .undefined: return Color(.white).opacity(0.75)
+            case .entertainment: return Color(uiColor: .systemPurple).opacity(0.75)
+            case .food: return Color(uiColor: .systemGreen).opacity(0.75)
+            case .home: return Color(uiColor: .systemYellow).opacity(0.75)
+            case .life: return Color(uiColor: .systemOrange).opacity(0.75)
             }
         }
     }
